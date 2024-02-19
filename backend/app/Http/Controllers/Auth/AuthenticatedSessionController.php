e2e427ed71d84cb2dbda0b26ae56bbba1ef20643
salmenkhelifi1 <salmen@base.tn>
2024-02-19 19:10:25 +0100

    login

--- backend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ backend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -7,19 +7,39 @@ use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
 
 class AuthenticatedSessionController extends Controller
 {
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request): JsonResponse
     {
-        $request->authenticate();
+        // $request->authenticate();
 
-        $request->session()->regenerate();
 
-        return response()->noContent();
+        // $request->session()->regenerate();
+
+        // return response()->noContent();
+
+        $credentials = $request->only('email', 'password');
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        // Retrieving the authenticated user
+        $user = Auth::user();
+
+        // Creating an API token for the user
+        $token = $user->createToken('api_token')->plainTextToken;
+
+        // Returning the user data and token as JSON response
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ]);
     }
 
     /**
